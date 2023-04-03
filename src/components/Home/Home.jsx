417129c81016d4c558e6a4bd51dcb63f5feac76c
mehedi-hasan16 <xmehedi3@gmail.com>
2023-04-03 21:20:24 +0600

    navigation spinner added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../Header/Header';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 
 const Home = () => {
+    const navigationSpinner = useNavigation();
     return (
         <div>
             <Header></Header>
+            <div>{navigationSpinner.state ==='loading'?'Loading...': ''}</div>
             <Outlet></Outlet>
         </div>
     );
