410fcd7c01218b3e4c38308d51bf3a5cb14d9586
mehedi-hasan16 <xmehedi3@gmail.com>
2023-04-03 21:34:47 +0600

    added grid style to friend list

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Friend from '../Friend/Friend';
+import './Friends.css'
 
 const Friends = () => {
     const userData = useLoaderData();
     return (
         <div>
             <h3>All friends: {userData.length}</h3>
-            {
-                userData.map(user=> <Friend user={user} key={user.id}></Friend>)
-            }
+            <div className='friend-list'>
+                {
+                    userData.map(user=> <Friend user={user} key={user.id}></Friend>)
+                }
+            </div>
         </div>
     );
 };
